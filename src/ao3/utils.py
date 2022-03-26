01033e71fbc8fda485323a79937b964ee4487a6c
Rae Knowler <daisywheel22@gmail.com>
2022-03-26 17:18:31 +0100

    Return user work count as int, not string

--- src/ao3/utils.py
+++ src/ao3/utils.py
@@ -255,7 +255,7 @@ def get_user_works_count(username, session):
     header_text = soup.h2.text
     m = re.search(WORKS_HEADER_REGEX, header_text)
 
-    return m.group(1)
+    return int(m.group(1))
 
 
 def get_series_info(series_id, session):
